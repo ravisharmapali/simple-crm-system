9801103cdbbdf954ea7a9210863259e417df24a9
ravi <ravisharmapali@gmail.com>
2023-05-02 18:24:39 +0530

    application created with customer entity and repo

---
+++ src/main/java/com/bytes/SimpleCrmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrmSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytes/SimpleCrmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
